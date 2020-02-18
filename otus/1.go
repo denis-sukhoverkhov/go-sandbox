04ae48520623b24669eb2fbc7633288dd508fe06
denis-sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2020-02-18 23:43:19 +0300

    программа печатающую текущее время / точное время с использованием библиотеки NTP.
    Программа должна корректно обрабатывать ошибки библиотеки: распечатывать их в STDERR и возвращать ненулевой код выхода.

---
+++ otus/1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+"fmt"
+"github.com/beevik/ntp"
+"log"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Текущее время: ", time)
+}
